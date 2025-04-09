969b3cedc305af6f4287a8dbbdf72a92100e1e8f
dominic <dominic@dmoser.com>
2025-04-10 01:44:35 +0200

    feat: add finished P03 project

--- P03_Dance/drone/main.py
+++ P03_Dance/drone/main.py
@@ -5,6 +5,21 @@ myDrone = Tello()
 myDrone.connect()
 myDrone.takeoff()
 
-# Neuen Code hier einfügen.
+# Schleife mit 3 Durchgängen.
+for i in range(3):
+    # Ausgabe des aktuellen Durchgangs.
+    print("Durchgang: " + str(i))
+    # Linkssalto ausfühern
+    myDrone.flip_left()
+    myDrone.move_back(20)
+    # Rückwärtssalto ausfühern
+    myDrone.flip_back()
+    myDrone.move_left(20)
+    # Rechtssalto ausfühern
+    myDrone.flip_right()
+    myDrone.move_right(20)
+    # Vorwärts ausfühern
+    myDrone.flip_forward()
+    myDrone.move_forward(20)
 
 myDrone.land()
