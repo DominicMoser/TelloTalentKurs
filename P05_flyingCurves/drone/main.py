8117197ad7cc1bdf7d78ef18a0b9aee7696b5442
dominic <dominic@dmoser.com>
2025-04-10 02:32:29 +0200

    feat: add finished P05 project

--- P05_flyingCurves/drone/main.py
+++ P05_flyingCurves/drone/main.py
@@ -3,6 +3,13 @@ from djitellopy import Tello
 myDrone = Tello()
 myDrone.connect()
 
+# Beispiel einer möglichen Kurve. Für ein besseres 3 Dimensionales Verständniss,
+# können die Kurven über geogebra gezeichnet werden
+# Fliege von der aktuellen Positon zu der Position 100, 0, 0 über die Position 50, 50, 0 
+# mit einer Geschwindigkeit von 30.
+# Hierbei fliegt die Drohne nur eine 2 Dimensionale Kurve.
+myDrone.curve_xyz_speed(50,50,0, 100,0,0, 30)
+
 myDrone.takeoff()
 
 myDrone.land()
