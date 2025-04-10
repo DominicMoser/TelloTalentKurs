02937465e4e4f50fe3e2c58d5e825f19ad046ede
dominic <dominic@dmoser.com>
2025-04-10 03:27:31 +0200

    feat: add finished P06 project

--- P06_catchMidAir/drone/main.py
+++ P06_catchMidAir/drone/main.py
@@ -4,3 +4,14 @@ myDrone = Tello()
 myDrone.connect()
 
 myDrone.takeoff()
+
+# Setzen der Distanz ab welcher die Drohne landen soll.
+landing_threashold = 8
+
+# Endlosschleife
+while True:
+    # Wenn die Distanz kleiner als landing_threashold ist, soll die Schleife beendet werden.
+    if myDrone.get_distance_tof() < landing_threashold:
+        break
+
+myDrone.land()
